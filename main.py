dd87201ca4faacebafb91a2cde38467cc6575919
孙鹏 <patrick.sunpeng@gmail.com>
2022-12-15 21:43:48 +0800

    init

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
+my_text = "China will continue to play its role as president of COP15, closely cooperate with all parties and stakeholders toward the conclusion of the Post-2020 Global Biodiversity Framework and successful completion of all items on COP15's agenda, and contribute to the full attainment of the three main objectives of the Convention on Biological Diversity — conservation, sustainable use and sharing of the benefits of biodiversity, Mao said."
+char_count = {c : my_text.count(c) for c in my_text}
+print (char_count)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
