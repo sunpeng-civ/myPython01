b5397e3c232c0e7435bd228e4d08470a7a7d7424
孙鹏 <patrick.sunpeng@gmail.com>
2022-12-16 19:28:30 +0800

    root

--- main.py
+++ main.py
@@ -8,6 +8,42 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
+import math, time
+
+accu = 0.000000000001
+x = eval(input("input num:"))
+root_num = eval(input("input root num:"))
+if x > 0:
+    left = 0
+    right = x
+else:
+    left = x
+    right = 0
+
+time1 = time.time()
+root = 0
+
+while 1:
+    root = float((left + right) / 2)
+    if root**root_num > x :
+        right = root
+    else:
+        left = root
+
+    if abs(root**root_num - x ) < accu:
+        break
+time2 = time.time()
+print ("the", root_num, "root of", x, "is ", root )
+print ('calc time is ',(time2-time1))
+
+
+sum = x**2
+time3 = time.time()
+
+print ("sqr of10", x ,"is ", sum)
+
+print ('sqr time is ',(time3-time2))
+
 my_text = "China will continue to play its role as president of COP15, closely cooperate with all parties and stakeholders toward the conclusion of the Post-2020 Global Biodiversity Framework and successful completion of all items on COP15's agenda, and contribute to the full attainment of the three main objectives of the Convention on Biological Diversity — conservation, sustainable use and sharing of the benefits of biodiversity, Mao said."
 char_count = {c : my_text.count(c) for c in my_text}
 print (char_count)
